4f604c0127d3fb102ec1d26f69102ade2a05ea43
Rafael <barto_et@hotmail.com>
2022-10-26 20:18:41 -0500

    updated confluent cli, disabled plugins

--- build/install_confluent.sh
+++ build/install_confluent.sh
@@ -2,9 +2,12 @@
 set -euo pipefail
 # Get Binary
 get_confluent() {
-  curl -sL --http1.1 https://cnfl.io/cli | sh -s -- v2.23.0
+  curl -sL --http1.1 https://cnfl.io/cli | sh -s -- v2.30.1
   mv -v bin/confluent /usr/local/bin/confluent
   chmod +x /usr/local/bin/confluent
+  mkdir ~/.confluent
+  touch ~/.confluent/config.json
+  echo '"disable_plugins: true"' > ~/.confluent/config.json
   echo "Confluent downloaded"
 }
 get_confluent
